49c9c6873ba3a8c6d00a0911dd255bbff2d6f45e
MaShaoming <SMa4@slb.com>
2016-10-27 18:47:22 +0800

    Clean

--- HelloWorld/HelloWorld/Startup.cs
+++ HelloWorld/HelloWorld/Startup.cs
@@ -6,22 +6,9 @@ namespace HelloWorld
 {
     public partial class Startup
     {
-        Private branch change.
         public void Configuration(IAppBuilder app)
         {
-		Master change.
-
             ConfigureAuth(app);
         }
-
-        Master change 1.
-        Master change 2.
-
-        Master change 3.
-        Master change 4.
-        Private change 5.
-        Private change 6.
-        Master change 5.
-        Master change 6.
     }
 }
